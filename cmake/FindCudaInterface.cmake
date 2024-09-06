112bdbe946c2fa47fe2daedef45bd67361358bf6
cianciosa <m4c@ornl.gov>
2024-09-06 12:38:45 -0400

    Macro defines need the c-preprocessor.

--- cmake/FindCudaInterface.cmake
+++ cmake/FindCudaInterface.cmake
@@ -10,27 +10,34 @@ find_package_handle_standard_args (CudaInterface
                                    REQUIRED_VARS CUDAToolkit_FOUND)
 
 if (NOT TARGET CudaInterface::CudaInterface)
-   message (STATUS "Setup CudaInterface")
+    message (STATUS "Setup CudaInterface")
 
-   add_library (CudaInterface::CudaInterface INTERFACE IMPORTED)
+    add_library (CudaInterface::CudaInterface INTERFACE IMPORTED)
 
-   target_link_libraries (CudaInterface::CudaInterface
+    target_link_libraries (CudaInterface::CudaInterface
 
-                          INTERFACE
+                           INTERFACE
 
-                          CUDA::cusolver
-   )
-
-   target_compile_definitions (CudaInterface::CudaInterface
-
-                               INTERFACE
-
-                               cusolverStatus_t=INTEGER\(C_SIZE_T\)
-                               cusolverDnHandle_t=TYPE\(C_PTR\)
-                               cusolverDnParams_t=TYPE\(C_PTR\)
-                               cudaDataType=INTEGER\(C_SIZE_T\)
-                               cublasStatus_t=INTEGER\(C_SIZE_T\)
-                               cublasLtHandle_t=TYPE\(C_PTR\)
+                           CUDA::cusolver
     )
+
+    target_compile_options (CudaInterface::CudaInterface
+
+                            PRIVATE
+
+                            $<$<COMPILE_LANGUAGE:Fortran>:-cpp>
+    )
+
+    target_compile_definitions (CudaInterface::CudaInterface
+
+                                INTERFACE
+
+                                cusolverStatus_t=INTEGER\(C_SIZE_T\)
+                                cusolverDnHandle_t=TYPE\(C_PTR\)
+                                cusolverDnParams_t=TYPE\(C_PTR\)
+                                cudaDataType=INTEGER\(C_SIZE_T\)
+                                cublasStatus_t=INTEGER\(C_SIZE_T\)
+                                cublasLtHandle_t=TYPE\(C_PTR\)
+     )
 endif ()
 
